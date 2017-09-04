2779815731902a580694185439adfa82cc03aad9
Luoana B <luoanab@gmail.com>
2017-09-04 09:20:41 +0300

    main js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     console.log("yo");
-    var operationState;
+    var operationState,
+        thresholds,
+        sensorValues;
     
     $.ajax({
         url: "sensors/operation"
@@ -11,6 +13,18 @@ $(document).ready(function(){
         } else {
             $('#automatic-mode').bootstrapToggle('on');
         }
+    });
+    
+    $.ajax({
+        url: "thresholds/all"
+    }).done(function(response){
+        thresholds = response.data.operation_mode;
+    });
+    
+    $.ajax({
+        url: "sensors"
+    }).done(function(response){
+        sensorValues = response.data;
     });
     
     bindEvents();
@@ -24,6 +38,34 @@ function bindEvents() {
             data: {
                 mode: $('#automatic-mode').prop("checked") ? "AUTO" : "MANUAL"
             }
+        }).done(function(){
+            //
+        }).fail(function(){
+            //
         });
     });
+     
+    $('.threshold-submit').click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        
+        var form = $(this).parent("form");
+        
+        $.ajax({
+            url: "threshold/"+form.attr("id"),
+            method: "PUT",
+            data: {
+                lightUpperValue:form.find(".light-upper-value").val(),
+                lightLowerValue: form.find(".light-upper-value").val(),
+                tempUpperValue: form.find(".temp-upper-value").val(),
+                tempLowerValue: form.find(".temp-upper-value").val(),
+                moistureUpperValue: form.find(".moisture-upper-value").val(),
+                moistureLowerValue: form.find(".moisture-upper-value").val(),
+            }
+        }).done(function(){
+            //
+        }).fail(function(){
+            //
+        });
+    })
 }
