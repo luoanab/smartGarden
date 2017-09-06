502900c7a32aa67cb565214922db5cdf3585efa0
Luoana B <luoanab@gmail.com>
2017-09-06 21:50:56 +0300

    organize code

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,31 @@
-$(document).ready(function(){
-    console.log("yo");
-    var operationState,
-        thresholds,
-        sensorValues;
-    
+var operationState,
+    thresholds,
+    sensorValues;
+
+$(document).ready(function() {
+    getOperation();
+    getAllThresholds();
+    getSensorsData();
+    bindEvents();
+})
+
+function getSensorsData() {
+    $.ajax({
+        url: "sensors"
+    }).done(function(response){
+        sensorValues = response.data;
+    });
+}
+
+function getAllThresholds() {
+    $.ajax({
+        url: "thresholds/all"
+    }).done(function(response){
+        thresholds = response.data.operation_mode;
+    });
+}
+
+function getOperation() {
     $.ajax({
         url: "sensors/operation"
     }).done(function(response){
@@ -14,58 +36,47 @@ $(document).ready(function(){
             $('#automatic-mode').bootstrapToggle('on');
         }
     });
-    
+}
+
+function handleAutomaticModeChange(e) {
     $.ajax({
-        url: "thresholds/all"
-    }).done(function(response){
-        thresholds = response.data.operation_mode;
+        url: "sensors/operation",
+        method: "PUT",
+        data: {
+            mode: $('#automatic-mode').prop("checked") ? "AUTO" : "MANUAL"
+        }
+    }).done(function(){
+        //
+    }).fail(function(){
+        // display error here
     });
-    
+}
+
+function handleShresholdSumbit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var form = $(this).parent("form");
+
     $.ajax({
-        url: "sensors"
-    }).done(function(response){
-        sensorValues = response.data;
+        url: "thresholds/"+form.attr("id"),
+        method: "PUT",
+        data: {
+            lightUpperValue:form.find(".light-upper-value").val(),
+            lightLowerValue: form.find(".light-upper-value").val(),
+            tempUpperValue: form.find(".temp-upper-value").val(),
+            tempLowerValue: form.find(".temp-upper-value").val(),
+            moistureUpperValue: form.find(".moisture-upper-value").val(),
+            moistureLowerValue: form.find(".moisture-upper-value").val(),
+        }
+    }).done(function(){
+        //
+    }).fail(function(){
+        // display error here
     });
-    
-    bindEvents();
-})
+}
 
 function bindEvents() {
-    $('#automatic-mode').change(function() {
-        $.ajax({
-            url: "sensors/operation",
-            method: "PUT",
-            data: {
-                mode: $('#automatic-mode').prop("checked") ? "AUTO" : "MANUAL"
-            }
-        }).done(function(){
-            //
-        }).fail(function(){
-            //
-        });
-    });
-     
-    $('.threshold-submit').click(function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        
-        var form = $(this).parent("form");
-        
-        $.ajax({
-            url: "thresholds/"+form.attr("id"),
-            method: "PUT",
-            data: {
-                lightUpperValue:form.find(".light-upper-value").val(),
-                lightLowerValue: form.find(".light-upper-value").val(),
-                tempUpperValue: form.find(".temp-upper-value").val(),
-                tempLowerValue: form.find(".temp-upper-value").val(),
-                moistureUpperValue: form.find(".moisture-upper-value").val(),
-                moistureLowerValue: form.find(".moisture-upper-value").val(),
-            }
-        }).done(function(){
-            //
-        }).fail(function(){
-            //
-        });
-    })
+    $('#automatic-mode').change(handleAutomaticModeChange(e));
+    $('.threshold-submit').click(handleShresholdSumbit(e));
 }
