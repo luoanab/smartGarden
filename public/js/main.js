5955c3ec57c803ad82d0299c72faac141b35c86d
Luoana B <luoanab@gmail.com>
2017-09-06 22:04:08 +0300

    hbs

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,20 @@ function getAllThresholds() {
     $.ajax({
         url: "thresholds/all"
     }).done(function(response){
-        thresholds = response.data.operation_mode;
+        thresholds = response.data;
+        //$('form.threshold');
+        
+        var form1 = $("#1").html();
+        // Compile the template data into a function
+        var templateScript = Handlebars.compile(form1);
+
+        var context = thresholds["1"];
+
+        // html = 'My name is Ritesh Kumar. I am a developer.'
+        var html = templateScript(context);
+
+        // Insert the HTML code into the page
+        form1.html(html);
     });
 }
 
