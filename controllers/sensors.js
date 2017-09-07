403857f17f5941af5bc2e49570bc0c07c84d46f2
Luoana B <luoanab@gmail.com>
2017-09-07 04:10:25 +0300

    hbs

--- controllers/sensors.js
+++ controllers/sensors.js
@@ -193,13 +193,11 @@ function read_sensor_values () {
 }
 
 function thresholds() {
-    thresholdsController.getThresholds: function(data, callback) {
-        dbController.getThresholds(null, function(err, response) {
-            console.log("got thresholds...: ", response); 
-            thresholds = response;
-            return;
-        })
-    }
+    thresholdsController.getThresholds(null, function(err, response) {
+        console.log("got thresholds...: ", response); 
+        thresholds = response;
+        return;
+    })
 }
 
 function auto_mode (lum_threshold, hum_threshold_down, hum_threshold_up, temp_threshold_down, temp_threshold_up) {
